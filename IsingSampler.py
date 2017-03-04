faf86aeb02c3813b1682dcf0d2d2c55481c885fa
vvoelz <vvoelz@gmail.com>
2017-03-04 16:00:17 -0500

    stand-alone sampler and trajectory objects from the notebooks

---
+++ IsingSampler.py
@@ -0,0 +1,252 @@
+import numpy as np
+
+from IsingTrajectory import *
+
+class IsingSampler(object):
+    """An object for performing kinetic Monte Carlo sampling of the 
+    'electrical Ising' model. """
+    
+    def __init__(self, nx=20, ny=20, voltage=0.0, debug=False, initial_state=None, depsilon=20.0):
+        """Initialize the sampler object."""
+        
+        self.nx = nx  # number of cells in the x dimension
+        self.ny = ny  # number of cells in the y dimension
+        self.ncells = nx*ny   # total number of cells
+        
+        self.debug = debug
+        
+        # set electrical constants
+        self.gating_charge = 1.0  # units eu
+        self.dq = self.gating_charge/float(self.ncells)  # the activating charge of each cell
+        self.voltage = voltage
+        self.k_Boltzmann = 0.086174 # Boltzmann's constant in meV/K
+        self.temperature = 290.0 # units K
+        self.kT          = self.k_Boltzmann*self.temperature  # 25 meV at 290 K
+        self.depsilon    = depsilon ## SHOULD be 24.0  # meV (this is equal to 2J of the standard Ising model) 
+        
+        # set kinetic Monte Carlo constants
+        self.pre_exponential_factor = 2.0e7 # units s^{-1}
+        self.Bronsted_slope = 0.5    #
+               
+        # initialize a random configuration of electric dipoles  e
+        if initial_state==None:
+            self.e = np.random.randint( 2, size=(self.nx, self.ny) )
+        else:
+            self.e = initial_state
+        if self.debug:
+            print 'self.e', self.e
+        
+        # compile a neighbor list for fast lookup
+        self.neighborlist = None
+        self.compile_neighborlist()
+        
+        
+        # initialize the number of activated nearest neighbors
+        self.n = self.count_all_activated_neighbors(self.e)   
+        if self.debug:
+            print 'self.n', self.n
+            
+        # Calculate the total energy from the states and their neighbors
+        self.energy = self.calc_total_energy(self.e, self.n)
+        if self.debug:
+            print 'self.energy', self.energy
+            print 'self.energy/self.kT', self.energy/self.kT
+
+        # The 0->1 activation energy depends *only* on the number of activated neighbors (0 through 4),
+        # which we can precompute.  
+        self.activation_energy = np.array([ (2.0*(2.0-na)*self.depsilon - 1.0*self.dq*self.voltage) for na in range(5)])
+        print 'self.activation_energy', self.activation_energy
+
+
+        # initialize rate constants for flipping a dipole
+        self.alpha_beta = self.calculate_all_alpha_beta(self.e, self.n)
+        if self.debug:
+            print 'self.alpha_beta', self.alpha_beta
+            
+            
+    def count_all_activated_neighbors(self, e):
+        """Count the numbers of north/south/east/west neighbors that
+        are activated."""
+        
+        n = np.zeros( e.shape, dtype=int) 
+        
+        for i in range(self.nx):      
+            for j in range(self.ny):
+                n[i,j] = self.count_activated_neighbors(e, i, j)
+        return n
+
+    def compile_neighborlist(self):
+        """Construct a lookup table for periodic-boundary neighbor indices."""
+        self.neighborlist = np.zeros( (self.nx, self.ny, 4, 2), dtype=int)
+            # [i,j,:,:]  = [[inorth, jnorth],
+            #               [isouth, jsouth],
+            #               [iwest,  jwest],
+            #               [ieast,  jeast]]
+
+        for i in range(self.nx):
+            for j in range(self.ny):
+                self.neighborlist[i,j,0,:] = [i, (j-1)%(self.ny)]
+                self.neighborlist[i,j,1,:] = [i, (j+1)%(self.ny)]
+                self.neighborlist[i,j,2,:] = [(i-1)%(self.nx), j]
+                self.neighborlist[i,j,3,:] = [(i+1)%(self.nx), j]
+
+    
+    def count_activated_neighbors(self, e, i, j):
+        """Count the number of activated neighbors for a particular i,j cell."""
+        
+        count = 0
+        #for k in range(4):
+        #    count += e[self.neighborlist[i,j,k,0],self.neighborlist[i,j,k,1]]       
+
+        count += e[self.neighborlist[i,j,0:4,0],self.neighborlist[i,j,0:4,1]].sum()       
+
+        return count
+
+    
+    def calc_total_energy(self, e, n):
+        """Calculate the energy of the input state
+        
+        INPUT
+        e     - an nx,ny array of 0 (resting) or 1 (activated)"""
+        
+        energy = 0.0
+        for i in range(self.nx):
+            for j in range(self.ny):
+                
+                energy +=  -1.0*self.dq*float(e[i,j])*self.voltage
+                
+                if e[i,j] == 0:
+                    # NOTE divide by 2 to avoid double-counting
+                    energy += n[i,j]*self.depsilon / 2.0 
+                else:
+                    energy += (4.0-n[i,j])*self.depsilon / 2.0 
+
+        return energy 
+ 
+
+    def calculate_all_alpha_beta(self, e, n):
+        """Calculate activating/deactivating rate constants for each cell"""
+        
+        alpha_beta = np.zeros( e.shape, dtype=float)
+        for i in range(self.nx):
+            for j in range(self.ny):
+                
+                if e[i,j] == 0:
+                    alpha_beta[i,j] = self.calculate_alpha(n, i, j)
+                else:
+                    alpha_beta[i,j] = self.calculate_beta(n, i, j)
+                    
+        return alpha_beta
+                
+    
+    
+    def calculate_alpha(self, n, i, j):
+        """Calculate activating rate constant 0->1 for a particular i,j cell"""   
+        return self.pre_exponential_factor*np.exp(-self.Bronsted_slope*self.activation_energy[n[i,j]]/self.kT)
+    
+
+    def calculate_beta(self, n, i, j):
+        """Calculate de-activating rate constant 1->0 for a particular i,j cell"""    
+        return self.pre_exponential_factor*np.exp((1.-self.Bronsted_slope)*self.activation_energy[n[i,j]]/self.kT)
+    
+    
+    
+    def sample(self, nsteps, print_every=10000, save_every=1000, energy_check=True):
+        """Perform kinetic Monte Carlo """
+        
+        nframes = nsteps/save_every 
+        self.t = IsingTrajectory(nframes, nx=self.nx, ny=self.ny)
+                        
+        time = 0.0
+        tau  = 0.0
+        
+        for step in range(nsteps):
+            
+            ###########################
+            ##### save a snapshot #####
+            
+            fwd_rate = self.alpha_beta[(self.e==0)].sum()
+            back_rate = self.alpha_beta[(self.e==1)].sum()
+                        
+            # append a snapshot to the the trajectory
+            if step%save_every == 0:
+                self.t.tally(self.e, time, tau, self.energy, fwd_rate, back_rate)
+            
+            if step%print_every == 0:
+                print 'step', step, 'of', nsteps,
+                if energy_check:
+                    print '| energy (on-the-fly total-recalc)', self.energy, self.calc_total_energy(self.e, self.n)
+                else:
+                    print
+
+            ###############################
+            
+            # Sum all the previously assigned rate constants ai together to obtain the total rate constant a
+            # of leaving the current configuration. 
+            total_rate_a = fwd_rate + back_rate
+            # total_rate_a = self.alpha_beta.sum()
+                    
+            # Then pick a uniform random number r1
+            r1 = np.random.rand()
+            # ... to draw a dwell time from an exponential distribution
+            # using the formula:
+            tau = -np.log(r1)/total_rate_a
+
+            # Pick a second random number r2 in order to determine which cell to flip.
+            # Each cell has probability ai/a of being chosen.
+            if (0):
+                p = self.alpha_beta.ravel()/total_rate_a
+                r2 = np.random.rand()
+                flip_index = -1
+                p_cumulative = 0.0
+                while r2 > p_cumulative:
+                    p_cumulative += p[flip_index+1]
+                    flip_index += 1
+                if self.debug:
+                    print 'r2', r2, 'flip_index', flip_index,
+            else:
+                # use fast numpy routines
+                flip_index = np.searchsorted(np.cumsum(self.alpha_beta.ravel()/total_rate_a), [np.random.rand()])[0]
+                #print 'flip_index', flip_index
+            iflip, jflip = np.unravel_index(flip_index, (self.nx,self.ny))
+            if self.debug:
+                print 'iflip, jflip', iflip, jflip
+                
+            # update the energy
+            if self.e[iflip,jflip] == 1:  # the move will be 1->0
+                self.energy -= self.activation_energy[ self.n[iflip,jflip] ]
+                #print 'iflip,jflip', iflip,jflip
+                #print 'self.n[iflip,jflip]', self.n[iflip,jflip]
+                #print 'self.activation_energy[ self.n[iflip,jflip] ]', self.activation_energy[ self.n[iflip,jflip] ]
+            else: # the move will be 0->1
+                self.energy += self.activation_energy[ self.n[iflip,jflip] ]
+
+            # flip the selected cell
+            self.e[iflip,jflip] = int(self.e[iflip,jflip]==0)
+            
+
+            
+            # update the neighboring neighbor counts
+            if self.e[iflip,jflip] == 1:  # the flip is 0 --> 1; add +1 to neighbor counts
+                #for k in range(4):
+                #    self.n[ self.neighborlist[iflip,jflip,k,0],self.neighborlist[iflip,jflip,k,1]] += 1
+                self.n[ self.neighborlist[iflip,jflip,0:4,0],self.neighborlist[iflip,jflip,0:4,1]] += 1      
+
+            else:  # the flip is 0 --> 1; subtract 1 from neighbor counts
+                #for k in range(4):
+                #    self.n[ self.neighborlist[iflip,jflip,k,0],self.neighborlist[iflip,jflip,k,1]] -= 1
+                self.n[ self.neighborlist[iflip,jflip,0:4,0],self.neighborlist[iflip,jflip,0:4,1]] -= 1 
+            
+            # update the rates for the flipped cell,          
+            if self.e[iflip,jflip] == 0:
+                self.alpha_beta[iflip,jflip] = self.calculate_alpha(self.n, iflip, jflip)
+            else:
+                self.alpha_beta[iflip,jflip] = self.calculate_beta(self.n, iflip, jflip)
+                
+            # update current time
+            time += tau
+
+                    
+        return self.t
+    
+    
---
+++ IsingTrajectory.py
@@ -0,0 +1,97 @@
+import numpy as np
+import os, sys
+
+class IsingTrajectory(object):
+    """An object to hold kinetic Monte Carlo trajectories (non-discrete time jumps), 
+    with methods to convert to discrete-time MSM trajectories."""
+    
+    def __init__(self, nframes=None, nx=20, ny=20):
+        """Initialize the trajectory object.
+        PARAMETERS
+        nframes    - if None, then do not allocate arrays
+                   - if supplied, allocate arrays of this length"""
+        
+        self.nframes = nframes
+        self.nx = nx
+        self.ny = ny
+        
+        self.stats_header = None
+        self.stats = None
+        
+        self.traj_header = None
+        self.traj = None
+        
+        if (nframes != None) and (nx != None) and (ny != None):
+            self.allocate_frames(nframes, nx, ny)
+        
+    def allocate_frames(self, nframes, nx, ny):
+        """Allocate a trajectory arrays of the right size"""
+
+        import numpy as np
+        
+        self.nframes = nframes
+        self.nx = nx
+        self.ny = ny
+        
+        self.stats_header = '%d %d %d\ntime(s)\tdwell_time(s)\tenergy(meV)\tfwd_rate(Hz)\tback_rate(Hz)'%(self.nframes, self.nx, self.ny)
+        self.stats = np.zeros( (nframes, 5), dtype=float)  
+        
+        self.traj_header = '%d frames of (%d x %d = %d cells)'%(nframes, nx, ny, nx*ny)
+        self.traj = np.zeros( (nframes, nx*ny), dtype=bool)
+        
+        self.current_frame = 0
+
+    def tally(self, state, time, dwell_time, energy, fwd_rate, back_rate):
+        """Write a frame."""
+        
+        self.stats[self.current_frame,:] = [time, dwell_time, energy, fwd_rate, back_rate]
+        self.traj[self.current_frame,:] = state.ravel().astype(bool)
+        self.current_frame += 1
+        
+    
+    def save(self, name):
+        """Save <name>.stats.npy and <name>.traj.npy to file"""
+        
+        outfile_stats = name + '.stats.dat'
+        np.savetxt(outfile_stats, self.stats, header=self.stats_header, fmt='%10.8e')
+        print 'Wrote', outfile_stats
+        
+        outfile_traj = name + '.traj.npy'
+        np.save(outfile_traj, self.traj)
+        print 'Wrote', outfile_traj
+
+        
+    def load(self, name):
+        """Load <name>.stats.npy and <inname>.traj.npy"""
+        
+        # parse filenames
+        infile_stats = name + '.stats.dat'   
+        infile_traj = name + '.traj.npy'
+        
+        # get header info
+        fin = open(infile_stats, 'r')
+        header_lines = [fin.readline() for i in range(2)]
+        #print header_lines
+        fin.close()
+        self.stats_header = header_lines[0] + header_lines[1]
+        #print 'self.stats_header', self.stats_header  
+        
+        # get array sizes and allocate arrays
+        fields = header_lines[0].strip('#').split()
+        nframes, nx, ny = [int(fields[i]) for i in [0,1,2]]
+        #print 'nframes, nx, ny', nframes, nx, ny
+        self.allocate_frames(nframes, nx, ny)
+        print 'Read', infile_stats
+        
+        # read in stats
+        self.stats = np.loadtxt(infile_stats)
+        
+        # read in trajectory data   
+        self.traj = np.load(infile_traj).astype(int)
+        print 'Read', infile_traj
+        
+        self.nframes = self.traj.shape[0]
+        self.current_frame = self.traj.shape[0]
+        
+
+
